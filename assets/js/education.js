2d17314e34e31e449f5d3786753063961c3d06db
Muhammad Huzaifa <121419942+MHUZAIFAM@users.noreply.github.com>
2024-10-19 16:04:15 +0500

    Update education.js

--- assets/js/education.js
+++ assets/js/education.js
@@ -4,67 +4,46 @@ AOS.init();
 
 const moocs = document.querySelector(".moocs");
 const moocscards = [
-  {
-    title: "Data Science",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/browse/data-science",
-  },
-  {
-    title: "Cryptography",
-    cardImage: "assets/images/education-page/udemy.svg",
+    {
+    title: "Introduction To Machine Learning",
+    cardImage: "assets/images/education-page/COURSERA.jpg",
     moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
   },
   {
-    title: "Machine Learning",
-    cardImage: "assets/images/education-page/coursera2.svg",
+    title: "Introduction To Artificial Intelligence",
+    cardImage: "assets/images/education-page/COURSERA.jpg",
     moocLink: "https://www.coursera.org/learn/machine-learning",
   },
   {
-    title: "Introduction to HTML 5",
-    cardImage: "assets/images/education-page/udacity.svg",
+    title: "Applied AI with DeepLearning",
+    cardImage: "assets/images/education-page/COURSERA.jpg",
     moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
   },
   {
-    title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity.svg",
+    title: "Introduction to Tensorflow for AI for Machine Learning and Deep Learning",
+    cardImage: "assets/images/education-page/COURSERA.jpg",
     moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
   },
   {
-    title: "Javascript",
-    cardImage: "assets/images/education-page/udacity.svg",
+    title: "AI in Healthcare Capstone",
+    cardImage: "assets/images/education-page/STANFORD.png",
     moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
   },
   {
-    title: "Bootstrap 4",
-    cardImage: "assets/images/education-page/udemy.svg",
+    title: "Fundamentals of Machine Learning for Healthcare",
+    cardImage: "assets/images/education-page/STANFORD.png",
     moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
   },
   {
-    title: "Intro to React",
-    cardImage: "assets/images/education-page/edx.svg",
+    title: "Introduction to Healthcare",
+    cardImage: "assets/images/education-page/STANFORD.png",
     moocLink: "https://www.edx.org/learn/reactjs",
   },
   {
-    title: "Intro to React Native",
-    cardImage: "assets/images/education-page/coursera2.svg",
+    title: "MATLAB ONRAMP",
+    cardImage: "assets/images/education-page/MATLAB.jpg",
     moocLink:
       "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
-  },
-  {
-    title: "NodeJS, Express and MongoDB",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/server-side-nodejs",
-  },
-  {
-    title: "XML-AJAX",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
-  },
-  {
-    title: "Data Structures & Algorithms",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink:
-      "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
   },
 ];
 
